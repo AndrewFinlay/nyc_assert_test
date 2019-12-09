5fa017c1c79d363d8dd170ce10396e716e11b366
Andrew Finlay <andrewf@blackmagicdesign.com>
2019-12-10 09:24:17 +1100

    Update tests to highlight cause of issue
    It seems that the cause of this bug is due to a problem that NodeJS has when trying to parse specific minified source.
    The tests have been updated to show this in action, three tests are run in a nyc/mocha environment.
    The first against the uninstrumented code, the second against pre-instrumented code taken from nyc's cache, the third is the pre-instrumented code run through atom-beautify to give it human readable spacing.
    The tests show that the first and third tests pass while the second test should fail.  It fails because the differences in whitespace cause node to emit different exception messages.

--- test/tests.js
+++ test/tests.js
@@ -1,28 +1,62 @@
 'use strict';
 
+// Run against npm target 'cover_no_instrument' under node v10+ to see the issue.
+
 const assert = require('assert');
 const myAssert = require('../lib/assert');
+const myInstrumentedAssert = require('../lib/instrumented_assert');
+const myUglyInstrumentedAssert = require('../lib/ugly_instrumented_assert');
 
 const NodeVersionMajor = /^v(\d+)\.(\d+)\.(\d+)/.exec(process.version)[1];
+console.log(`Test running on node v${NodeVersionMajor}`);
+
 const errRegEx = (NodeVersionMajor < 10)
   ? /false == true/
   : /evaluated to a falsy value/;
+const errMsg = (NodeVersionMajor < 10)
+  ? "false == true"
+  : "evaluated to a falsy value";
 
-const failAssert = () => { myAssert(false, 'OK: explicitly throwing'); };
-
-console.log(`Test running on node v${NodeVersionMajor}`)
-
-describe('assert', function() {
-  it('check existing assert behaviour', function(done) {
-    myAssert(true);
+describe('Assert', function() {
+  it(`assert responds with "${errMsg}"`, function() {
+    const failAssert = () => myAssert(false);
 
     try
     {
       assert.throws(failAssert, errRegEx);
-      done();
     }
     catch(ex) {
-      done(new Error('Unexpected exception: ' + ex));
+      throw new Error('Unexpected exception: ' + ex);
     }
   });
 });
+
+describe('Ugly Pre-instrumented Assert', function() {
+    it(`assert responds with "${errMsg}"`, function() {
+        const failAssert = () => myUglyInstrumentedAssert(false);
+
+        try
+        {
+            assert.throws(failAssert, errRegEx);
+        }
+        catch(ex) {
+            throw new Error('Unexpected exception: ' + ex);
+        }
+    });
+});
+
+// This code has been run through Atom beautify
+describe('Pre-instrumented Assert', function() {
+  it(`assert responds with "${errMsg}"`, function() {
+    const failAssert = () => myInstrumentedAssert(false);
+
+    try
+    {
+      assert.throws(failAssert, errRegEx);
+    }
+    catch(ex) {
+      throw new Error('Unexpected exception: ' + ex);
+    }
+  });
+});
+
